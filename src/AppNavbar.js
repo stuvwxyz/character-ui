04498d190aade48a48b7665a7bb65aca6febcb90
stuvwxyz <stuvwxyzz@gmail.com>
2019-03-01 17:20:52 -0600

    Revert "Updated home page"

--- src/AppNavbar.js
+++ src/AppNavbar.js
@@ -7,7 +7,6 @@ import { Collapse,
     NavItem,
     NavLink,
     } from 'reactstrap';
-import { Link } from 'react-router-dom';
 
 export default class AppNavbar extends Component {
     constructor(props) {
@@ -24,7 +23,7 @@ export default class AppNavbar extends Component {
 
     render() {
         return <Navbar color="dark" dark expand="md">
-            <NavbarBrand tag={Link} to="/">Home</NavbarBrand>
+            <NavbarBrand></NavbarBrand>
             <NavbarToggler onClick={this.toggle}/>
             <Collapse isOpen={this.state.isOpen} navbar>
                 <Nav className="ml-auto" navbar>
